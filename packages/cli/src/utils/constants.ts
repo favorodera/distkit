05af653218a6f1d9f6dddc03307c3e5c4849843e
Favour Emeka <favorodera@gmail.com>
2026-08-23 18:55:25 +0100

    feat(cli): define shared constants and command args
    - Add default registry URL and configuration names
    - Provide reusable common arguments for commands

---
+++ packages/cli/src/utils/constants.ts
@@ -0,0 +1,27 @@
+import type { EnumArgDef, PositionalArgDef, StringArgDef } from 'citty'
+import { basename } from 'pathe'
+import { version } from '../../package.json'
+
+/** The default registry name. */
+export const defaultRegistryName = 'distkit'
+
+/** The default registry index URL. */
+export const defaultRegistryIndexUrl = `https://raw.githubusercontent.com/favorodera/distkit/refs/tags/v${version}/packages/registry/src/index.json`
+
+/** The name of the user configuration file. */
+export const userConfigFileName = 'distkit.config.ts'
+
+/** The base name of the user configuration file. */
+export const userConfigFileBaseName = basename(userConfigFileName, '.ts')
+
+/** Common arguments for commands. */
+export const commonArgs: Record<string, EnumArgDef | PositionalArgDef | StringArgDef> = {
+  registry: {
+    alias: 'r',
+    default: defaultRegistryName,
+    description: 'Registry name',
+    required: true,
+    type: 'string',
+    valueHint: 'registry-name',
+  },
+}
